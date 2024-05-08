319fd756916ae4380a4c04e154c15b5c98f54a97
williamwen2 <william_wen_2024@163.com>
2024-05-08 16:15:30 +0800

    springboot starter

--- order-ddd/order-application/src/main/java/com/order/poc/boot/OrderStarter.java
+++ order-ddd/order-application/src/main/java/com/order/poc/boot/OrderStarter.java
@@ -1,4 +1,12 @@
 package com.order.poc.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class OrderStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderStarter.class, args);
+    }
 }
